96358ae1f28c7163b432d3acff4b2a600fcc3bd1
Nikhil Gowda <nikhil.gowda@nih.gov>
2018-06-15 14:08:10 -0400

    Scripts for multi-sample data
    Added cca and multiclustering, as well as call scripts for both.
    
    Next feature for this branch is differential expression by sample contrast.

---
+++ scrna_cca_call.R
@@ -0,0 +1,11 @@
+## grab args
+args <- commandArgs(trailingOnly = TRUE)
+DIR <- args[1]
+# Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+setwd(DIR) # new 
+rmarkdown::render("scrna_cca.Rmd", output_file=paste0("scrna_cca.html"), params = list(
+  seurat = as.character(unlist(strsplit(args[2], split=" "))),
+  contrasts = as.character(unlist(strsplit(args[3], split=" "))),
+  projectId = args[4],
+  projectDesc = args[5]
+))
