e10e252a19f1b4de8ef5976389af212062be0dba
Nikhil Gowda <nikhil.gowda@nih.gov>
2018-06-29 12:50:17 -0400

    call script bugfixes

--- scrna_initial_call.R
+++ scrna_initial_call.R
@@ -3,11 +3,12 @@ args <- commandArgs(trailingOnly = TRUE)
 DIR <- args[1]
 # Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
 setwd(DIR) # new 
-rmarkdown::render(args[4],"_scrna_initial.Rmd", params = list(
+rmarkdown::render("scrna_initial.Rmd", output_file=paste0(args[4],"_scrna_initial.html"), params = list(
     matrix = args[2],
     species = args[3],
     projectId = args[4],
     projectDesc = args[5],
-    mattype = args[6]
+    mattype = args[6],
+    doCycleRegress = args[7]
   ))
 
--- scrna_jackstraw_call.R
+++ scrna_jackstraw_call.R
@@ -3,7 +3,7 @@ args <- commandArgs(trailingOnly = TRUE)
 DIR <- args[1]
 # Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
 setwd(DIR) # new 
-rmarkdown::render(args[3],"_scrna_jackstraw.Rmd", params = list(
+rmarkdown::render("scrna_jackstraw.Rmd", output_file=paste0(args[3],"_scrna_jackstraw.html"), params = list(
     seurat = args[2],
     projectId = args[3],
     projectDesc = args[4]
