7f25ed5ec83bbd406a6d7a01aabc2b01afeed0db
Nikhil Gowda <nikhil.gowda@nih.gov>
2018-07-18 13:57:21 -0400

    setting environment variable for pandoc in all call scripts

--- scrna_cca_call.R
+++ scrna_cca_call.R
@@ -1,7 +1,7 @@
 ## grab args
 args <- commandArgs(trailingOnly = TRUE)
 DIR <- args[1]
-# Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+Sys.setenv(RSTUDIO_PANDOC="/usr/local/apps/rstudio/rstudio-1.1.447/bin/pandoc/")#COMMENT OUT/CHANGE AS NEEDED
 setwd(DIR) # new 
 rmarkdown::render("scrna_cca.Rmd", output_file=paste0(args[4],"_scrna_cca.html"), params = list(
   seurat = as.character(unlist(strsplit(args[2], split=" "))),
--- scrna_multicluster_call.R
+++ scrna_multicluster_call.R
@@ -1,7 +1,7 @@
 ## grab args
 args <- commandArgs(trailingOnly = TRUE)
 DIR <- args[1]
-# Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+Sys.setenv(RSTUDIO_PANDOC="/usr/local/apps/rstudio/rstudio-1.1.447/bin/pandoc/")#COMMENT OUT/CHANGE AS NEEDED
 setwd(DIR) # new 
 rmarkdown::render("scrna_multicluster.Rmd", output_file=paste0(args[5],"_scrna_multicluster_",args[3],"_",args[4],".html"), params = list(
   seurat = args[2],
